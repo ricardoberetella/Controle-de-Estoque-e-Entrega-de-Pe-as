4329e29f0717b087593551a1856d6ac0b0578a70
ricardoberetella <ricardoberetella20@gmail.com>
2026-02-05 10:54:16 -0300

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -4,11 +4,14 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyB0i38lQMhE9UgUIh5rqmZAuu1Z-KXUcI0",
   authDomain: "controle-de-demonstracao.firebaseapp.com",
+  databaseURL: "https://controle-de-demonstracao-default-rtdb.firebaseio.com",
   projectId: "controle-de-demonstracao",
   storageBucket: "controle-de-demonstracao.firebasestorage.app",
   messagingSenderId: "804320803586",
-  appId: "1:804320803586:web:e418e8e9c0b5adb881a01b"
+  appId: "1:804320803586:web:e418e8e9c0b5adb881a01b",
+  measurementId: "G-GFCPSFCFJF"
 };
 
 const app = initializeApp(firebaseConfig);
+// Exportamos o db_firestore para o dataService usar
 export const db_firestore = getFirestore(app);
