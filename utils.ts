adbfb6121820bb2c1a7f497d5c29445fec9a3473
ricardoberetella <ricardoberetella20@gmail.com>
2026-02-05 15:34:00 -0300

    Delete utils.ts

--- utils.ts
+++
@@ -1,8 +0,0 @@
-
-export const generateId = () => Math.random().toString(36).substring(2, 9).toUpperCase();
-
-export const formatDate = (dateStr: string) => {
-  if (!dateStr) return '';
-  const date = new Date(dateStr);
-  return date.toLocaleDateString('pt-BR');
-};
