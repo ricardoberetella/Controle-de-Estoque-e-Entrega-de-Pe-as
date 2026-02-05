01f6d49a963647db9edfcc560d71dbc520944260
ricardoberetella <ricardoberetella20@gmail.com>
2026-02-05 16:12:02 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const rootElement = document.getElementById('root');
+const container = document.getElementById('root');
 
-if (!rootElement) {
-  console.error("Erro: Elemento root não encontrado no HTML.");
-} else {
-  const root = ReactDOM.createRoot(rootElement);
+if (container) {
+  const root = ReactDOM.createRoot(container);
   root.render(<App />);
+} else {
+  console.error("Elemento root não encontrado.");
 }
