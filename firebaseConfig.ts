04fb29fbd133695bb2880ea46cd0d3cf689dec94
ricardoberetella <ricardoberetella20@gmail.com>
2026-02-05 16:15:44 -0300

    Update firebaseConfig.ts

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,20 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
-// Suas credenciais do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyB0i38lQMhE9UgUIh5rqmZAuu1Z-KXUcI0",
-  authDomain: "controle-de-demonstracao.firebaseapp.com",
-  databaseURL: "https://controle-de-demonstracao-default-rtdb.firebaseio.com",
-  projectId: "controle-de-demonstracao",
-  storageBucket: "controle-de-demonstracao.firebasestorage.app",
-  messagingSenderId: "804320803586",
-  appId: "1:804320803586:web:e418e8e9c0b5adb881a01b",
-  measurementId: "G-GFCPSFCFJF"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
-
-// Exporta a instância do Realtime Database para ser usada no dataService
 export const rtdb = getDatabase(app);
